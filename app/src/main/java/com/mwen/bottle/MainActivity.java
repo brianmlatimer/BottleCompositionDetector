b97a1e7fb62060d697a4fcb5d83b8f5ae37c8c24
brian l <brianmlatimer@gmail.com>
2019-11-02 02:14:32 -0500

    This is brian first commit

--- app/src/main/java/com/mwen/bottle/MainActivity.java
+++ app/src/main/java/com/mwen/bottle/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Brian Code
+        // Brian Code on his own branch
 
-        // Matthew Wen
     }
 }
