232e65a30b571a4f3fdd78ad90975bd530bd96ff
brian l <brianmlatimer@gmail.com>
2019-11-02 04:22:28 -0500

    Save image as a bmp file

--- app/src/main/java/com/mwen/bottle/MainActivity.java
+++ app/src/main/java/com/mwen/bottle/MainActivity.java
@@ -1,9 +1,17 @@
 package com.mwen.bottle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,16 +21,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED)
+//        {
+//            ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.CAMERA}, requestCode);
+//        }
         Button b = findViewById(R.id.button1);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                    startActivity(intent);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (intent.resolveActivity((getPackageManager())) != null) {
+                        startActivityForResult(intent, 1); // 1 equals to request image capture
+                    }
             }
         });
         // Brian Code on his own branch
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Log.v("RESULT", "It worked, we got an image");
+            Bundle bundle = data.getExtras();
+            Bitmap imagebmp = (Bitmap) bundle.get("data"); 
+        }
     }
 }
 
